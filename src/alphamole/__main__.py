6d9615e9233b63bfd3637f9a1cfd65db6a2d0bc8
Marc <marc.broghammer@gmx.de>
2022-03-21 15:12:22 +0100

    Models cumulated in colored_prots.pse; delete inbetween.

--- src/alphamole/__main__.py
+++ src/alphamole/__main__.py
@@ -1,5 +1,6 @@
 """Command-line interface."""
 import click
+from pymol import cmd
 
 from . import alter_chains
 from . import chainbows_and_bfactors
@@ -16,7 +17,9 @@ def main() -> None:
     remove_gly_linker.remove_gly_linker("ranked_*_gly.pdb", output="{name}_-gly.pdb")
     alter_chains.alter_chains("ranked_*_-gly.pdb", output="{name}_-gly.pdb")
     super_pdbs.super_pdbs("ranked_*_-gly.pdb", output="{name}_-gly.pdb")
+    cmd.delete("*")
     chainbows_and_bfactors.chainbows_and_bfactors("ranked_*_-gly.pdb")
+    cmd.delete("*")
     color_prots.color_prots("ranked_*_-gly.pdb")
 
 
